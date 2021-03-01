d16b199b9d5a973777a92eba1906ab0d02b896d0
chunxue.jiao <chunxue.jiao@mall-ai.com>
2021-03-01 17:17:38 +0800

    webpack-代码分离

--- 工程化/webpack-demo/src/print.js
+++
@@ -1,4 +0,0 @@
-export default function printMe() {
-  // console.log(a);
-  console.log('I get called from print.js!');
-}
--- 工程化/webpack-demo/webpack.config.js
+++ 工程化/webpack-demo/webpack.config.js
@@ -1,27 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin') // 生成html，然后将打包文件自动引入到index.html中
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
   mode: 'development',
- entry: {
-   index: './src/index.js',
-   print: './src/print.js',
- },
- devtool: 'inline-source-map',
- devServer: {
-    contentBase: './dist',
- },
- plugins: [
-  // new CleanWebpackPlugin(),
-  new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-  new HtmlWebpackPlugin({
-    title: 'Development',
-  }),
-],
-  output: {
-   filename: '[name].bundle.js',
-   path: path.resolve(__dirname, 'dist'),
-   publicPath: '/'
+  entry: {
+    index: './src/index.js',
   },
-  
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
