c54ec68b7e1e799ddb50a1f3d0f16826dfaf15ba
chunxue.jiao <chunxue.jiao@mall-ai.com>
2021-03-01 17:34:36 +0800

    update webpack-cache

--- 工程化/webpack-demo/src/index.js
+++ 工程化/webpack-demo/src/index.js
@@ -1,16 +1,14 @@
+import _ from 'lodash';
+//  import Print from './print';
 
- function getComponent() {
-  return import('lodash')
-    .then(({ default: _ }) => {
-      const element = document.createElement('div');
+  function component() {
+    const element = document.createElement('div');
 
-      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // lodash 是由当前 script 脚本 import 进来的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //  element.onclick = Print.bind(null, 'Hello webpack!');
 
-      return element;
-    })
-    .catch((error) => 'An error occurred while loading the component');
- }
+    return element;
+  }
 
- getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+  document.body.appendChild(component());
