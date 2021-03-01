77974cc52ce0f47d765f7312b553e0a10c258f9c
chunxue.jiao <chunxue.jiao@mall-ai.com>
2021-03-01 11:54:41 +0800

    update webpack--管理输出

--- 工程化/webpack-demo/src/index.js
+++ 工程化/webpack-demo/src/index.js
@@ -1,40 +1,18 @@
-
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
+import printMe from './print.js';
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-function component() {
-  const element = document.createElement('div');
+  element.appendChild(btn);
 
-  // lodash，现在通过一个 script 引入
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+   return element;
+ }
 
-   // 将图像添加到我们已经存在的 div 中。
-   const myIcon = new Image();
-   myIcon.src = Icon;
- 
-   element.appendChild(myIcon);
-
-   
-   console.log(Data);
-   console.log(Notes);
-
-  return element;
-}
-
-document.body.appendChild(component());
+ document.body.appendChild(component());
